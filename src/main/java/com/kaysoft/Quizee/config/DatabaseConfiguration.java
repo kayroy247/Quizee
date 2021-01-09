e5d81a8217342d5f0ea97b1829f55380c0037ba1
kayroy247 <okunladekayode@gmail.com>
2021-01-09 22:03:06 +0100

    add database connection

---
+++ src/main/java/com/kaysoft/Quizee/config/DatabaseConfiguration.java
@@ -0,0 +1,21 @@
+package com.kaysoft.Quizee.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/quizee");
+        builder.username("postgres");
+        builder.password("root");
+        System.out.println("My custom datasource bean has been initialized and set");
+        return builder.build();
+
+    }
+}
