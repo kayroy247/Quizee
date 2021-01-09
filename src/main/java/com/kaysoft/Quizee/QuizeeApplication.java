29a9a49c5f7d3305f8bb1ddf041c6ccb0b82f7cc
kayroy247 <okunladekayode@gmail.com>
2021-01-09 17:26:40 +0100

    first commit

---
+++ src/main/java/com/kaysoft/Quizee/QuizeeApplication.java
@@ -0,0 +1,13 @@
+package com.kaysoft.Quizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizeeApplication.class, args);
+	}
+
+}
